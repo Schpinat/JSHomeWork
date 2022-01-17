80a3057f00f84c5b723e2d0d90566ccd36f212f9
Stanislav Derkach <Sderkach1987@gmail.com>
2022-01-17 15:41:31 +0200

    init commit

---
+++ task 1.js
@@ -0,0 +1,16 @@
+/*
+Перепишите код, заменив оператор `if` на тернарный оператор `?`
+
+```js
+var result;
+if (a + b < 4) {
+result = true;
+} else {
+result = false;
+}
+```
+*/
+var a = 2;
+var b = 1;
+var result = (a + b < 4) ? true : false;
+    console.log(result);
---
+++ task 3.js
@@ -0,0 +1,35 @@
+/*
+Перепишите код, заменив оператор `switch` на оператор `if..else`:
+
+```js
+var val;
+switch (val) {
+case 'a':
+console.log( 'a' );
+break;
+
+case 'b':
+case 'c':
+case 'd':
+case 'e':
+console.log( 'others' );
+break;
+
+default:
+console.log( 'unknown' );
+}
+```
+*/
+var val = 'c';
+
+if (val == 'a') {
+    console.log( 'a' );
+} 
+else if (val == 'b' || val == 'c' || val == 'd' || val == 'e')
+{
+    console.log( 'others' );
+} 
+else
+{
+    console.log( 'unknown' );
+}
